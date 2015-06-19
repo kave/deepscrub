c3d4ea5d6d4fc55612b5b2f4867f22290c64aa3d
etapau <etapau@gmail.com>
2015-06-19 10:14:34 -0400

    initial commit

--- sapo/settings/base.py
+++ sapo/settings/base.py
@@ -104,8 +104,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'sapo.urls'
@@ -140,11 +138,6 @@ INSTALLED_APPS = (
 EMAIL_SUBJECT_PREFIX = '[sapo] '
 
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
-
-# django-debug-toolbar settings
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
